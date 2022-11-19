9e74f12b72ad003d24ea7f671f8331196933e90d
Anna Franczyk <annafranczyk@MBP-Anna.(none)>
2022-11-19 15:05:58 +0100

    Added new page

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
-echo "Hello there";
+$zmienna="tekst";
+echo "<h2>Przykładowy \n $zmienna!</h2>";
+echo "To ", "jest ", "przykładowy ", "$zmienna. \n"; print "Jeszcze jeden $zmienna\n";
+echo print "Jeszcze jeden $zmienna\n";
+
+?>
